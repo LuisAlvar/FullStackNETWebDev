5eb8575b3a501df1399a42e152008bd13cec7bf0
luis <alvarez.en.luis@gmail.com>
2024-12-26 21:29:47 -0800

    implementation of Serilog, but not tested

--- WorldCities/WorldCitiesAPI/Program.cs
+++ WorldCities/WorldCitiesAPI/Program.cs
@@ -1,9 +1,25 @@
 using Microsoft.EntityFrameworkCore;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.MSSqlServer;
 using WorldCitiesAPI.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Adding Serilog support
+builder.Host.UseSerilog((ctx, lc) => lc
+  .ReadFrom.Configuration(ctx.Configuration)
+  .WriteTo.MSSqlServer(
+    connectionString: ctx.Configuration.GetConnectionString("DefaultConnnection"),
+    restrictedToMinimumLevel: LogEventLevel.Information,
+    sinkOptions: new MSSqlServerSinkOptions {
+      TableName = "LogEvents",
+      AutoCreateSqlTable = true,
+    })
+  .WriteTo.Console()
+);
 
 builder.Services.AddControllers().AddJsonOptions(options => {
   options.JsonSerializerOptions.WriteIndented = true;
@@ -17,6 +33,9 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
   options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
+
+// adding logging HTTP request middleware support
+app.UseSerilogRequestLogging(); 
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
