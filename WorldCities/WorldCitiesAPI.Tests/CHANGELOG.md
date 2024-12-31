5165af14a15e351a93c4f7f42947fb0b2f3580da
luis <alvarez.en.luis@gmail.com>
2024-12-30 22:51:16 -0800

    working on the unit testing the API

---
+++ WorldCities/WorldCitiesAPI.Tests/CHANGELOG.md
@@ -0,0 +1,4 @@
+﻿- Run `dotnet new xunit -o WorldCitiesAPUI.Tests`
+- Add Nuget package `Moq --version 4.20.72`
+- Add Nuget package `Microsoft.EntityFrameworkCore.InMemory --version 6.0.36`
+- Add WorldCitiesAPI project as a dependences to WorldCitiesAPI.Tests
---
+++ WorldCities/WorldCitiesAPI.Tests/CitiesController_Test.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorldCitiesAPI.Controllers;
+using WorldCitiesAPI.Data;
+using WorldCitiesAPI.Data.Models;
+
+namespace WorldCitiesAPI.Tests
+{
+  public class CitiesController_Test
+  {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    [Fact]
+    public async Task GetCity()
+    {
+      /// Arrange
+      /// todo: define the required assets
+      var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+        .UseInMemoryDatabase(databaseName: "WorldCities")
+        .Options;
+      using var context = new ApplicationDbContext(options);
+      context.Add(new City()
+      {
+        Id = 1,
+        CountryId = 1,
+        Lat = 1,
+        Lon = 1,
+        Name = "TestCity1"
+      });
+      context.SaveChanges();
+
+      var mockILogger = new Mock<ILogger<CitiesController>>();
+      ILogger<CitiesController> logger = mockILogger.Object;
+
+      var controller = new CitiesController(logger, context);
+      City? city_existing = null;
+      City? city_notExisting = null;
+
+      /// Act 
+      city_existing = (await controller.GetCity(1)).Value;
+      city_notExisting = (await controller.GetCity(2)).Value;
+
+      /// Assert
+      Assert.NotNull(city_existing);
+      Assert.Null(city_notExisting);
+    }
+  }
+}
